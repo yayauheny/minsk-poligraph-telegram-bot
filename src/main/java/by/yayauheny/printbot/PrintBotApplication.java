3eab0c1eb0eae5a3607b56c50fd956e079b64952
yayauheny <work.evles@gmail.com>
2023-12-03 18:10:45 +0300

    Initial commit

---
+++ src/main/java/by/yayauheny/printbot/PrintBotApplication.java
@@ -0,0 +1,13 @@
+package by.yayauheny.printbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/yayauheny/printbot/PrintBotApplicationTests.java
@@ -0,0 +1,13 @@
+package by.yayauheny.printbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
